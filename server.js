be27d9f55fd2d12037491183fd94c93dbf7d6b18
ifotn <Rich.Freeman@GeorgianCollege.ca>
2024-01-15 10:50:51 -0500

    l2/3 - add async to callbacks for axios

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 let http = require('http');
 let fs = require('fs');
 
-http.createServer((req, res) => {
+http.createServer(async (req, res) => {
     switch (req.url) {
         case '/css/bootstrap.min.css': 
             fs.readFile('css/bootstrap.min.css', 'utf8', (err, cssContents) => {
@@ -10,13 +10,20 @@ http.createServer((req, res) => {
             });
             break;
         default: 
-            fs.readFile('index.html', 'utf8', (err, fileContents) => {
+            fs.readFile('index.html', 'utf8', async (err, fileContents) => {
                 if (err) {
                     console.log(err);
                 }
                 else {
                     // send html to browser
                     res.write(fileContents);
+
+                    // axios api call goes here...don't forget to import axios above
+                    /* code here - you need 3 lines:
+                    1 - call API using axios using await keyword
+                    2 - output <h2>
+                    3 - output api data using JSON.stringify()
+                    */
                     res.end();
                 }
             });
